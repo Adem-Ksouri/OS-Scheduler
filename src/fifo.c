949532b52974f9e7721e679ccf47d54d4f31a1a7
Adem Ksouri <ksouriadem44@gmail.com>
2025-11-14 22:45:02 +0100

    Refactor scheduler to be in C programming language (#6)

---
+++ src/fifo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/scheduler.h"
+
+execute* fifo_scheduler(process* processes, int n, int* out_count) {
+    qsort(processes, n, sizeof(process), compare_process);
+
+    execute* result = (execute*)malloc(n * sizeof(execute));
+    if (!result) return NULL;
+
+    int ts = 0;
+    for (int i = 0; i < n; i++) {
+        if (ts < processes[i].arrival)
+            ts = processes[i].arrival;
+
+        int te = ts + processes[i].exec_time;
+
+        result[i].p = processes[i];
+        result[i].ts = ts;
+        result[i].te = te;
+        result[i].events = processes[i].events;
+
+        ts = te;
+    }
+
+    *out_count = n;
+    return result;
+}
