e71d565ce473ddc8a0299b9d2bf6a04400b7c80a
Adem-Ksouri <ksouriadem44@gmail.com>
2025-11-14 22:55:35 +0100

    Reorganize code in specific folders

--- include/scheduler.h
+++ include/scheduler.h
@@ -4,37 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-// event type
-typedef struct {
-    int t;  // after t seconds of execution, the process will print a comment
-    char comment[250];
-} event;
-
-// process type
-typedef struct{
-    int pid;
-    int ppid;
-    char name[20];
-    int arrival;
-    int exec_time;
-    int priority;
-    int nbEvents;
-    event* events;
-} process;
-
-// execute type
-typedef struct {
-    process p;
-    int ts;
-    int te;
-    event* events;
-} execute;
-
-int compare_event(const void* a, const void* b);
-event* getEvents(process p, int tl, int tr, int *out_cnt);
-int compare_process(const void* a, const void* b);
-execute make_execute(process p, int tl, int tr, event* events);
+#include "utils.h"
 
 execute* fifo_scheduler(process* processes, int n, int *out_cnt);
 execute* rr_scheduler(process* processes, int n, int *out_cnt);
--- unit_tests/fifo_unit_tests/fifo_scheduler_test.c
+++ unit_tests/fifo_unit_tests/fifo_scheduler_test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/scheduler.h"
+#include "../../include/scheduler.h"
 
 int main() {
     freopen("unit_tests/output.txt", "w", stdout);
